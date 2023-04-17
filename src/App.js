67914d994e7f61add1debd3fc3652d3495f8ca6f
naterocks49 <108183408+naterocks49@users.noreply.github.com>
2023-04-17 14:51:41 -0400

    Added pages and routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import { Routes, Route, Link } from 'react-router-dom';
+import routes from './Routes';
+
 function App() {
-    return (
-        <div>
-            <h1>App is connected.</h1>
-        </div>
-    );
-};
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
+      </nav>
 
+      <Routes>
+        {routes.map((route, index) => (
+          <Route key={index} path={route.path} component={route.component} exact />
+        ))}
+      </Routes>
+    </div>
+  );
+}
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from "react";
-import App from './App.js';
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom';
+import App from './App.js';
 
 const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
 
 root.render(
-        <App />
+    <BrowserRouter>
+        <App />   
+    </BrowserRouter>
+
 );
