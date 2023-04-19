36765b142e65047fe5ca24e48824618ba6b83ae5
naterocks49 <108183408+naterocks49@users.noreply.github.com>
2023-04-19 09:49:12 -0400

    Added right nav button

--- src/components/RightOption.js
+++ src/components/RightOption.js
@@ -1,5 +1,24 @@
-function RightOption() {
-    return <div>Right Option</div>;
-}
+import { useState, useEffect } from "react";
+
+
+function RightOption({ menu, page, handlePage, index }) {
+    const [nextPage, setnextPage] = useState(menu[index + 1])
+    const [nextPageIndex, setnextPageIndex] = useState(index + 1)
+
+    console.log(nextPage)
+    console.log(nextPageIndex)
+
+    const handleClick = () => {
+        handlePage(nextPage)
+        setnextPageIndex(nextPageIndex + 1)
+        setnextPage(menu[nextPageIndex + 1])
+    }
+
+    return (
+        <div>
+            {(nextPageIndex < Object.keys(menu).length - 1) && <button onClick={handleClick}>Go Back!</button>}
+        </div>
+    );
+};
 
 export default RightOption
