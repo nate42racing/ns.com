51a9e4efedbd2dd0f75984112fa30c46def8f249
naterocks49 <108183408+naterocks49@users.noreply.github.com>
2023-04-22 15:45:32 -0400

    Updates

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,13 +1,25 @@
 import { useState } from "react";
+import { MdDensityMedium } from 'react-icons/md';
 
 function Dropdown({ navitems }) {
     const [isOpen, setIsOpen] = useState(false);
 
+    const renderedNavItems = navitems.map((option) => {
+        return (
+            <div key={option.value}>
+                {option.label}
+            </div>
+        );
+    });
 
+    const handleClick = () => {
+        setIsOpen((currentIsOpen) => !currentIsOpen);
+    };
 
     return (
         <div>
-
+            <MdDensityMedium onClick={handleClick} />
+            {isOpen && renderedNavItems}
         </div>
     )
 };
