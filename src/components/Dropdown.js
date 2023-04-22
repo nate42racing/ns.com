bd505a363b4dda4bb6092f6f8c0732072e4fa4d7
naterocks49 <108183408+naterocks49@users.noreply.github.com>
2023-04-22 15:36:55 -0400

    Added dropdown comp

---
+++ src/components/Dropdown.js
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function Dropdown({ navitems }) {
+    const [isOpen, setIsOpen] = useState(false);
+
+
+
+    return (
+        <div>
+
+        </div>
+    )
+};
+
+export default Dropdown;
