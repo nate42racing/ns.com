b302acb211849608b57e29eefff8cc7ad0002027
naterocks49 <108183408+naterocks49@users.noreply.github.com>
2023-04-22 18:43:42 -0400

    Added classnames

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,9 @@
-function Header() {
-    return <div></div>
+import classNames from 'classnames';
+
+function Header({ className }) {
+    const classes = classNames(className)
+
+    return <div className="bg-red-400">HEADER</div>
 };
 
 export default Header;
