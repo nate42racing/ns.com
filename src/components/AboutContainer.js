f1999f4f3793ef840eb37299e66f7e3352301e10
naterocks49 <108183408+naterocks49@users.noreply.github.com>
2023-04-29 16:10:51 -0400

    Updates

--- src/components/AboutContainer.js
+++ src/components/AboutContainer.js
@@ -1,5 +1,24 @@
-function AboutContainer() {
-    return <div>Container</div>
+import { GoDeviceDesktop } from 'react-icons/go'
+import { AiOutlineDatabase } from 'react-icons/ai'
+import { AiOutlineCloudDownload } from 'react-icons/ai';
+import React from 'react';
+
+
+function AboutContainer({ details }) {
+    let icon = <GoDeviceDesktop />
+    if (details.icon === 'AiOutlineCloudDownload') {
+        icon = <AiOutlineCloudDownload />
+    };
+    if (details.icon === 'AiOutlineDatabase') {
+        icon = <AiOutlineDatabase />
+    };
+
+    return (
+        <div>
+            {icon}
+            <h3>{details.name}</h3>
+        </div>
+    );
 };
 
-export default AboutContainer
+export default AboutContainer;
--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,12 +1,26 @@
 import { motion } from "framer-motion";
+import AboutContainer from "../components/AboutContainer";
 
 function AboutPage() {
+    const details = [
+        {icon:'GoDeviceDesktop', name:'Front End', summary:'', languages:'', devtools:''},
+        {icon:'AiOutlineDatabase', name:'Back End', summary:'', languages:'', devtools:''}, 
+        {icon:'AiOutlineCloudDownload', name:'DevOps', summary:'', languages:'', devtools:''},  
+    ];
+
+    const renderedSections = details.map((section) => {
+        return <AboutContainer details={section} key={section.name}/>
+    });
+
     return (
         <motion.div exit={{ x: window.innerWidth, transition: {duration: 0.4 }  }} initial={{width: 0}} animate={{width: "100%"}}  className="flex justify-center">
-            <div>
+            <div className="flex flex-col items-center">
                 <h1 className="text-5xl text-black text-center font-semibold">About me</h1>
                 <p className="text-xl mx-20 mt-12">I'm a full-stack developer with over 10 years of experience writing code. I work with top level agencies around the world creating interactive and responsive web applications.</p>
                 <p className="text-xl mx-20 mt-8">I am always staying on the cutting edge of web technologies and I am always trying to push the envelope of what can be designed without sacraficing website performance and stability.</p>
+                <div className="flex justify-center flex-row mt-12">
+                    {renderedSections}
+                </div>
             </div>
         </motion.div>
     )
