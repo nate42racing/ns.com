a066d674e195e5a71bc86d72cafb8f25c923ec3b
naterocks49 <108183408+naterocks49@users.noreply.github.com>
2023-05-01 18:12:58 -0400

    styling

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,10 @@
 import Button from "./Button";
 
 function Card({ workData }) {
-    const classes = `bg-red-700/60  bg-cover flex flex-col grow-1 items-center justify-start h-64 border rounded-lg shadow hover:bg-red-700/40 hover:border-black group transition ease-in-out duration-500 delay-150`
+    const classes = `bg-red-700/40  bg-cover flex flex-col grow-1 items-center justify-start h-64 border rounded-lg shadow hover:bg-red-700/10 hover:border-black group transition ease-in-out duration-500 delay-150`
     return (
         <div className={classes}>
-            <p className="text-center text-2xl font-semibold mt-6">{workData.title}</p>
+            <p className="text-center text-gray-300 text-2xl font-semibold mt-6">{workData.title}</p>
             <p className="text-center text-sm md:text-lg  mb-2 mx-4">{workData.description}</p>
             <a href={workData.link}>
                 <Button outline className=" mb-4">{workData.buttontext}</Button>
