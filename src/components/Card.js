248f4acad3911b4308bc9dc982913f6dfdb6fd5f
naterocks49 <108183408+naterocks49@users.noreply.github.com>
2023-05-01 08:25:16 -0400

    Updates

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,13 @@
-function Card() {
-    return <div>Card</div>
+import Button from "./Button";
+
+function Card({ workData }) {
+    return (
+        <div className="flex flex-col items-center justify-end h-64 border rounded-lg shadow hover:bg-red-400 hover:border-black group">
+            <p className="text-center text-2xl font-semibold hidden group-hover:flex mx-4">{workData.title}</p>
+            <p className="text-center text-lg hidden group-hover:flex mb-2 mx-4">{workData.description}</p>
+            <Button outline className="hidden group-hover:flex mb-4">Click me</Button>
+        </div>
+        )
 };
 
 export default Card;
--- src/pages/WorkPage.js
+++ src/pages/WorkPage.js
@@ -3,15 +3,46 @@ import Card from "../components/Card";
 
 
 function WorkPage() {
+    const workData = [
+        {
+            title: 'Portfolio Website',
+            img: '/placeholder.img',
+            description: 'Reusable and responsive portfolio website created with React and AWS.',
+            link: 'link.com',
+        },
+        {
+            title: 'Detailing Quote Generator',
+            img: '/placeholder.img',
+            description: 'dasda',
+            link: 'link.com',
+        },
+        {
+            title: 'Meal Planner App',
+            img: '/placeholder.img',
+            description: 'dasda',
+            link: 'link.com',
+        },
+        {
+            title: 'Financial API',
+            img: '/placeholder.img',
+            description: 'dasda',
+            link: 'link.com',
+        },
+    ];
+
+    const renderedCards = workData.map((option) => {
+        return <Card workData={option} />
+    });
+
+
     return (
         <motion.div exit={{opacity:0}} initial={{opacity:0}} animate={{opacity:1}}>
-            <div>
-                <Card></Card>
-                <Card></Card>
-                <Card></Card>
-                <Card></Card>
-                <Card></Card>
-                <Card></Card>
+            <div className="flex flex-col items-center">
+                <h1 className="text-5xl text-black text-center font-semibold">My Work</h1>
+                <p className="text-xl mx-40 mt-12 text-center">I have worked with small and large companies to design beautiful sites, applications, and tools for a wide variety of uses.</p>
+            </div>
+            <div className="grid lg:grid-cols-2 xl:grid-cols-3 justify-items-stretch gap-8 mt-24">
+                {renderedCards}
             </div>
         </motion.div>
     )
