4746432365d4c68ddfddafd582df91129de7c4f7
naterocks49 <108183408+naterocks49@users.noreply.github.com>
2023-05-01 18:06:30 -0400

    check

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,11 @@
 import Button from "./Button";
 
 function Card({ workData }) {
-    const classes = `bg-${workData.img} bg-cover flex flex-col grow-1 items-center justify-start h-64 border rounded-lg shadow hover:bg-red-400 hover:opacity-60 hover:border-black group transition ease-in-out duration-500 delay-150`
+    const classes = `bg-red-700/60  bg-cover flex flex-col grow-1 items-center justify-start h-64 border rounded-lg shadow hover:bg-red-700/40 hover:border-black group transition ease-in-out duration-500 delay-150`
     return (
         <div className={classes}>
             <p className="text-center text-2xl font-semibold hidden group-hover:flex mt-6">{workData.title}</p>
-            <p className="text-center text-lg hidden group-hover:flex mb-2 mx-4">{workData.description}</p>
+            <p className="text-center text-sm md:text-lg hidden group-hover:flex mb-2 mx-4">{workData.description}</p>
             <a href={workData.link}>
                 <Button outline className="hidden group-hover:flex mb-4">{workData.buttontext}</Button>
             </a>
--- src/pages/WorkPage.js
+++ src/pages/WorkPage.js
@@ -40,16 +40,20 @@ function WorkPage() {
 
 
     return (
-        <motion.div exit={{opacity:0}} initial={{opacity:0}} animate={{opacity:1}}>
-            <div className="flex flex-col items-center">
-                <h1 className="text-5xl text-black text-center font-semibold">My Work</h1>
-                <p className=" text-lg md:text-xl mx-10 md:mx-40 mt-12 text-center">I have worked with small and large companies to design beautiful sites, applications, and tools for a wide variety of uses.</p>
+        <motion.div exit={{ opacity: 0 }} initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+          <div className="flex flex-col items-center">
+            <h1 className="text-5xl text-black text-center font-semibold">My Work</h1>
+            <p className="text-lg md:text-xl mx-10 md:mx-40 mt-12 text-center">
+              I have worked with small and large companies to design beautiful sites, applications, and tools for a wide variety of uses.
+            </p>
+          </div>
+          <div className="flex justify-center">
+            <div className="grid mx-10 lg:grid-cols-2 justify-items-stretch gap-8 mt-24 max-w-md min-w-full">
+              {renderedCards}
             </div>
-            <div className="grid mx-10 lg:grid-cols-2 justify-items-stretch gap-8 mt-24 max-w-4xl">
-                {renderedCards}
-            </div>
+          </div>
         </motion.div>
-    )
+    );
 };
 
 export default WorkPage;
